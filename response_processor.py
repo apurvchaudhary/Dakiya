ec3ecc87440c41a455845f6629b3cda6ad30a1c1
apurvchaudhary <apurv.sirohi@gmail.com>
2021-05-23 23:09:58 +0530

    allowed method in API

--- response_processor.py
+++ response_processor.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 INFO_MESSAGE = {
     "405": "405 wrong http method",
     "404": "404 url was not found",
+    "allowed_method": "\n\nAllowed method : {allowed}"
 }
 
 
@@ -21,6 +22,10 @@ class ResponseProcessor:
         if status_code == 200:
             return _response.text
         elif status_code == 405:
-            return self.show_info_box(title=INFO_MESSAGE["405"], info=_response.text)
+            return self.show_info_box(
+                title=INFO_MESSAGE["405"],
+                info=_response.text + INFO_MESSAGE["allowed_method"].format(
+                    allowed=_response.headers["Allow"])
+            )
         elif status_code == 404:
             return self.show_error_box(title=INFO_MESSAGE["404"], error=_response.text)
